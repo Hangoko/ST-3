f6f794cc772483aaa15d1c75dd1333bca35ad8dd
Зюзин Никита <launman@mail.ru>
2026-05-10 18:08:25 +0300

    Fix cpplint

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -14,7 +14,8 @@ void DoorTimerAdapter::Timeout() {
 }
 
 TimedDoor::TimedDoor(int timeout)
-    : adapter(new DoorTimerAdapter(*this)), iTimeout(timeout), isOpened(false) {}
+    : adapter(new DoorTimerAdapter(*this)), iTimeout(timeout),
+    isOpened(false) {}
 
 bool TimedDoor::isDoorOpened() {
   return isOpened;
